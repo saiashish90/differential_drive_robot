a2c5551aab8021e356053fd7c2e9d566be6f21a1
saiashish90 <saiashish90@gmail.com>
2019-07-09 00:42:35 +0530

    Kinetic version

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/sai/Desktop/robot/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/sai/Desktop/robot/simulation_ws/devel;/opt/ros/kinetic".split(';') if "/home/sai/Desktop/robot/simulation_ws/devel;/opt/ros/kinetic" != "" else []
